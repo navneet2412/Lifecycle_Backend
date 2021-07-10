23520d7f2cf4b985e63e1bf8ef6fa65008564059
Navneet Kumar <43682270+navneet2412@users.noreply.github.com>
2021-07-10 14:47:46 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -7,12 +7,6 @@ const sequelize = new Sequelize(database, user, password, {
   host: host,
 });
 
-// const sequelize = new Sequelize(
-//   // "mysql://b3ddecbc4ca42c:52f33026@eu-cdbr-west-03.cleardb.net/heroku_2e44c0e28ae22a5?reconnect=true",
-//   "mysql://qf4fqxspwp5fftnn:wtktn2czcwrgouqk@lmc8ixkebgaq22lo.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/kg61vx26e27m2n6v",
-//   {
-//     dialect: "mysql",
-//   }
-// );
+
 
 module.exports = sequelize;
